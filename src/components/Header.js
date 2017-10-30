445a22b0a2ebce43140c4fa7064f27bc6057b6ba
David Ryan <djrdev@icloud.com>
2017-10-29 22:37:53 -0600

    header start

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const Header = () => {
+  return(
+    <View style={{height: 30,backgroundColor: '#E2D9A2'}}>
+      <Text>legalBlocks</Text>
+    </View>
+  )
+}
+
+// const style = Stylesheet.create({
+//   headerContainer: {
+//     flex: 1,
+//     height: 30,
+//     backgroundColor: '#E2D9A2',
+//   },
+// })
+
+
+
+export default Header;
